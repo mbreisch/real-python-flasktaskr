b06c894b5bd52988b937f70f315856994e0ed0c9
Matthew B. Reisch <m.reisch@amalgamatedanalytics.com>
2016-01-21 13:03:20 -0500

    Refactored for sqlalchemy

--- project/_config.py
+++ project/_config.py
@@ -12,3 +12,6 @@ SECRET_KEY='\x07\xdd\xb7\x1e\xe8\x1d*\xe3`3\xa2\x9e\xf6\x94\xd4i\x14(\xa7\x93\xe
 # Define full path for the database
 DATABASE_PATH=os.path.join(basedir,DATABASE)
 
+# the database uri
+SQLALCHEMY_DATABASE_URI='sqlite:///'+DATABASE_PATH
+
--- project/db_create.py
+++ project/db_create.py
@@ -1,14 +1,10 @@
-import sqlite3
-from _config import DATABASE_PATH
+from views import db
+from models import Task
+from datetime import date
 
-with sqlite3.connect(DATABASE_PATH) as connection:
+db.create_all()
 
-    c=connection.cursor()
+db.session.add(Task("Finish this tutorial",date(2015,3,13),10,1))
+db.session.add(Task("Finish Real Python",date(2015,3,13),10,1))
 
-    # CREATE TABLE
-    c.execute("""CREATE TABLE IF NOT EXISTS tasks(task_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL,
-                  due_date TEXT NOT NULL, priority INTEGER NOT NULL, status INTEGER NOT NULL )""")
-
-    # INSERT dummy data
-    c.execute("INSERT INTO tasks(name,due_date,priority,status) VALUES('Finish this tutorial','01/20/2016',10,1)")
-    c.execute("INSERT INTO tasks(name,due_date,priority,status) VALUES('Eat lunch','01/19/2016',10,1)")
+db.session.commit()
